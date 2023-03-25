e10a4d6dfe3a888306a6bbe304ad8d8aca798c67
DESKTOP-IBJNLFM\MikeIt <mixaylo.skv@gmail.com>
2023-03-25 14:58:00 +0200

    express

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ const cors = require("cors");
 
 const app = express();
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  keepAlive: true,
-});
+mongoose
+  .connect(process.env.MongoDB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB", error);
+  });
 app.use(cors({ credentials: true, origin: true }));
 
 // Middleware for handling JSON requests
